0fe4c94ec931172a55e1b4ba9389057ce241c238
Leon <leon.perkovic95@gmail.com>
2018-03-15 22:53:07 +0100

    Initial project setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# Meta - Discord Bot
+
+### Requirements
+* [Java JDK](http://www.oracle.com/technetwork/java/javase/downloads/index.html)
+* [PostgreSQL](https://www.postgresql.org/download/)
+
+### Initial Setup
+1. Clone the repository: `git clone https://github.com/leon-perkovic/meta-discord-bot.git`
+2. Run `./gradlew clean build` in **/meta-discord-bot** folder
+
+### Postgres Setup
+ - *TBA ...*
---
+++ src/main/java/com/meta/leon/discordbot/DiscordBotApp.java
@@ -0,0 +1,46 @@
+package com.meta.leon.discordbot;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @author Leon, created on 12/03/2018
+ */
+public class DiscordBotApp extends ListenerAdapter{
+
+    public static void main(String[] args) throws LoginException, InterruptedException, IOException{
+
+        // load application.properties file
+        Properties prop = new Properties();
+        InputStream input = DiscordBotApp.class.getClassLoader().getResourceAsStream("application.properties");
+        prop.load(input);
+
+        // initialize Bot and set token using property value
+        JDA jdaBot = new JDABuilder(AccountType.BOT).setToken(prop.getProperty("discordApp.token")).buildBlocking();
+        jdaBot.addEventListener(new DiscordBotApp());
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event){
+
+        Message message = event.getMessage();
+        MessageChannel messageChannel = event.getChannel();
+        User user = event.getAuthor();
+
+        if(message.getContentRaw().startsWith("!")){
+            messageChannel.sendMessage("Hello " + user.getName() + ", that's a command :)").queue();
+        }
+    }
+
+}
